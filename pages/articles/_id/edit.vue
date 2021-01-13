3f5df546a8ede1c133203d7bb64f3821e3e1356a
tagucch <y.t.3k.gunners@gmail.com>
2021-01-13 16:16:20 +0900

    ディレクトリ修正

---
+++ pages/articles/_id/edit.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">sample-nuxt-blog</h1>
+      <input v-model="article.title" placeholder="title" />
+      <textarea v-model="article.body" placeholder="body"></textarea>
+      <button class="submit" @click="submit()">Submit</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+import { mapActions } from 'vuex';
+
+export default Vue.extend({
+  middleware: 'checkLogin',
+  async asyncData({ params }) {
+    const res = await axios.get(`http://localhost:3000/article/${params.id}`)
+    return { article: res.data.article }
+  },
+  data() {
+    return {
+      article: {
+        id: 0 as number,
+        title: '' as string,
+        body: '' as string,
+      }
+    }
+  },
+  methods: {
+    ...mapActions('article', ['updateArticle']),
+    async submit() {
+      const success = await this.updateArticle({ article: this.article })
+      if (success) {
+        this.$router.push('/')
+      }
+    },
+  },
+})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+input,
+textarea {
+  border: solid 1px black;
+}
+</style>
